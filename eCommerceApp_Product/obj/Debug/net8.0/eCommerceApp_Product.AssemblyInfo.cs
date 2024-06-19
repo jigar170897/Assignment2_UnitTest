bd81462f499498e6009cb49cb35acfc55e64164d
Jigar Rana <Jrana2073@conestogac.on.ca>
2024-06-19 17:51:32 -0400

    First commit

--- eCommerceApp_Product/obj/Debug/net8.0/eCommerceApp_Product.AssemblyInfo.cs
+++ eCommerceApp_Product/obj/Debug/net8.0/eCommerceApp_Product.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerceApp_Product")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d83cf4fa8fe303c44c3e9430a6b2d7866ca6da")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerceApp_Product")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerceApp_Product")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
